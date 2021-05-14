f8fa64adefad37b19ac79fb58b4327e0e474139e
= <jdavid@yext.com>
2021-05-14 07:49:15 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/africariyoki/precache-manifest.5548e6bd8dd5e82fa52cbd54156cfb48.js"
+  "/africariyoki/precache-manifest.5f4df932d7eb3998121f7236142b3236.js"
 );
 
 workbox.skipWaiting();
