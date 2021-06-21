1e03a75e932630df292c3819b4f4dbb35e65ae43
= <jdavid@yext.com>
2021-06-21 01:15:46 -0400

    Updates

---
+++ precache-manifest.415f17d2fad7a01ed75b6400cff06afe.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8218af99901f71f407d",
+    "url": "/static/js/app.36f1f268.chunk.js"
+  },
+  {
+    "revision": "e91487e8c8aa62ce91ce",
+    "url": "/static/js/runtime~app.34c76111.js"
+  },
+  {
+    "revision": "9dc6e4a624dfb49e24e6",
+    "url": "/static/js/2.51dd2337.chunk.js"
+  },
+  {
+    "revision": "631fe2578c08a9962b414069708b4ec8",
+    "url": "/static/media/logo.631fe257.png"
+  },
+  {
+    "revision": "ac0b83ee1fed4fb8472fb38d9e4e30b2",
+    "url": "/static/media/blankBack.ac0b83ee.jpeg"
+  },
+  {
+    "revision": "4aa88d029521a8c1b5d2ed3b6ad19cda",
+    "url": "/static/media/cloudx1.4aa88d02.png"
+  },
+  {
+    "revision": "fedbdd786da87d37e6decd9af38f666d",
+    "url": "/static/media/cloudx2.fedbdd78.png"
+  },
+  {
+    "revision": "3b47e00dd6455079621141d264943467",
+    "url": "/static/media/cloudx3.3b47e00d.png"
+  },
+  {
+    "revision": "87c467e69eae0e0d28dd62158fcc249b",
+    "url": "/static/media/cloudx4.87c467e6.png"
+  },
+  {
+    "revision": "dadbcb03ba1e7af319232c19c1321c30",
+    "url": "/static/media/cloudx5.dadbcb03.png"
+  },
+  {
+    "revision": "324a513cb9c683dc30777bdbccf59995",
+    "url": "/./fonts/JosefinSans-VariableFont_wght.ttf"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "cd4bf354f7d4a6bf36fa1a52c7dac4ac",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "5f9fe61e53eb91fe37b703df2e3f6ec0",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "027ce2dd8335d8c85a5d6befc40df778",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "599543b28eb4a22a4cc55ccab58e7913",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7719175572517ec3cc5977eb48ea8167",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  }
+];
