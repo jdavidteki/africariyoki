d49e4393462b256459e2e2ede271696ceb0f9350
= <jdavid@yext.com>
2022-04-06 20:52:46 -0400

    fix not routing on chrome ios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Admin from './components/admin/Admin.js'
-import GuessSong from './components/guessSong/GuessSong.js'
 import AnnotationFixer from './components/annotationFixer/AnnotationFixer.js'
-import PopularLine from './components/popularLine/PopularLine.js'
 import LRCFixer from './components/lrcFixer/LRCFixer.js'
 import ForOFor from './components/forofor/ForOFor.js';
-import CompleteTheLyrics from './components/completelyrics/CompleteLyrics.js';
 import Games from './components/games/Games.js';
 import ScoreboardGuessSong from './components/scoreboardGuessSong/ScoreboardGuessSong.js';
 import ScoreboardNextLine from './components/scoreboardNextLine/ScoreboardNextLine.js';
@@ -28,30 +25,28 @@ window.onresize();
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Homepage3} />
-          <Route path="/africariyoki" exact component={Homepage3} />
-          <Route path="/admin" exact component={Admin} />
-          <Route path="/guesssong" exact component={GuessTheSong} />
-          <Route path="/karaokedisplay/:id" component={Karaoke} />
-          <Route path="/lrcfixer/:id" component={LRCFixer} />
-          <Route path="/game" exact component={Games} />
-          <Route path="/guessthesong" exact component={GuessTheSong} />
-          <Route path="/popularline" exact component={PopularLines} />
-          <Route path="/cls" exact component={NextLine}/>
-          <Route path="/annotationfixer/:id" component={AnnotationFixer} />
-          <Route path="/scoreboardguesssong" component={ScoreboardGuessSong} />
-          <Route path="/scoreboardnextline" component={ScoreboardNextLine} />
-          <Route path="/scoreboardguesssongline" component={ScoreboardGuessSongLine} />
-          <Route path="/updatepopline" component={UpdatePopline} />
-          <Route path="/karaoke/:id" component={Karaoke} />
-          <Route path="/guess-the-song" component={GuessTheSong} />
-          <Route path="/popular-lines" component={PopularLines} />
-          <Route path="/next-line" component={NextLine} />
-          <Route component={ForOFor}/>
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/" exact component={Homepage3} />
+        <Route path="/africariyoki" exact component={Homepage3} />
+        <Route path="/admin" exact component={Admin} />
+        <Route path="/guesssong" exact component={GuessTheSong} />
+        <Route path="/karaokedisplay/:id" component={Karaoke} />
+        <Route path="/lrcfixer/:id" component={LRCFixer} />
+        <Route path="/game" exact component={Games} />
+        <Route path="/guessthesong" exact component={GuessTheSong} />
+        <Route path="/popularline" exact component={PopularLines} />
+        <Route path="/cls" exact component={NextLine}/>
+        <Route path="/annotationfixer/:id" component={AnnotationFixer} />
+        <Route path="/scoreboardguesssong" component={ScoreboardGuessSong} />
+        <Route path="/scoreboardnextline" component={ScoreboardNextLine} />
+        <Route path="/scoreboardguesssongline" component={ScoreboardGuessSongLine} />
+        <Route path="/updatepopline" component={UpdatePopline} />
+        <Route path="/karaoke/:id" component={Karaoke} />
+        <Route path="/guess-the-song" component={GuessTheSong} />
+        <Route path="/popular-lines" component={PopularLines} />
+        <Route path="/next-line" component={NextLine} />
+        <Route component={ForOFor}/>
+      </Switch>
     );
   }
 }
