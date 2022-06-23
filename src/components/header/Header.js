91fb32712d521ec027f14d00568fa4248926467e
= <jdavid@yext.com>
2022-06-22 21:33:05 -0400

    fix header hardrefresh issue

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
 
             if (localVersion == null || val > localVersion){
               localStorage.setItem('version', val);
-              window.location.href = window.location.href + `?hardrefresh=${val}`
+              window.location.href = window.location.href
             }
           }
         )
