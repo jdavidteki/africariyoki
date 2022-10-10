bfdda065c364b45ba54955e74aa1613866322559
= <jdavid@yext.com>
2022-10-10 15:14:44 -0400

    yokitweets added fix

--- src/components/YokiTweets/index.jsx
+++ src/components/YokiTweets/index.jsx
@@ -24,6 +24,20 @@ class ConnectedYokiTweets extends Component {
   }
 
   componentDidMount(){
+    //hack: use this to fix github pages doing ?/ on pages
+    if (window.location.href.includes("?/")){
+      let actualDestination = window.location.href.split("?/")[1]
+      if(this.props.history == undefined){
+        //TODO: figure out if it's possible to not have to do this
+        window.location.href = "/" + actualDestination
+      }else{
+        this.props.history.push({
+          pathname: "/" + actualDestination
+        });
+        window.location.reload(false);
+      }
+    }
+
     var urlParams = new URLSearchParams(window.location.search);
     var bckId = urlParams.get('bckId')
 
